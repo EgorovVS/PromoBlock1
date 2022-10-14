8db676919e9b98ef86bf1f8aa8f1b21c12366acf
Viktor Egorov <viktor.spoon2@gmail.com>
2022-10-14 12:40:57 +0400

    Initial commit

---
+++ src/main/java/MyList/Author.java
@@ -0,0 +1,5 @@
+package MyList;
+
+public interface Author {
+    String getAuthor();
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import MyList.MyList;
+
+public class Test {
+    public static void main(String[] args) {
+        MyList<String> myList = new MyList<String>();
+        System.out.println(myList.getAuthor());
+
+
+    }
+
+}
